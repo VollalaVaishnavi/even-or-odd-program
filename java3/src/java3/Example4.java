34099a2951312d85104400db4b9db0405629d2bc
vaishu vollala <vaishu vollala@LAPTOP-U09TFG76>
2022-02-09 23:03:02 +0530

    commit

---
+++ java3/src/java3/Example4.java
@@ -0,0 +1,15 @@
+package java3;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		 int num=11;
+		 if(num%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+			
+
+	}
+
+}
